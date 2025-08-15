2c24c2d43ef3e956055bc4b0019ba5b2be62bef1
LeenixP <lipeng.git@qq.com>
2025-08-15 18:34:26 +0800

    更新【1.69寸彩屏】模块

---
+++ 1-69-color-screen/src/BSP_Code/LCD/lcd_init.c
@@ -0,0 +1,241 @@
+#include "lcd_init.h"
+
+
+static volatile bool g_transfer_complete = false;
+void spi0_callback(spi_callback_args_t *arg)
+{
+	/* 如果是发送完成触发的中断则将发送完成标志位置1 */
+    if(SPI_EVENT_TRANSFER_COMPLETE == arg->event)
+        g_transfer_complete = true;
+}
+
+/******************************************************************************
+      函数说明：LCD串行数据写入函数
+      入口数据：dat  要写入的串行数据
+      返回值：  无
+******************************************************************************/
+void LCD_Writ_Bus(u8 dat)
+{
+    uint8_t recv_data = 0;
+	fsp_err_t err = FSP_SUCCESS;
+	volatile uint32_t wTimeout = 10000; // 10ms
+
+	LCD_CS_Clr();
+
+	/* 数据发送 */
+    err = R_SPI_WriteRead(&g_spi0_ctrl,
+							&dat, 		 // 数据发送
+						  	NULL,        // 数据接收
+							1,           // 发送长度
+							SPI_BIT_WIDTH_8_BITS);
+	if (err != FSP_SUCCESS)
+	{
+		printf("\r\nSPI发送错误\r\n");
+		return;
+	}
+
+	/* 等待发送成功 */
+	while (false == g_transfer_complete)
+	{
+		if (wTimeout-- == 0)
+		{
+			g_transfer_complete = false; // 重置标志位
+			printf("\r\nSPI发送超时\r\n");
+			return;
+		}
+		delay_us(1);
+	}
+	g_transfer_complete = false; // 重置标志位
+
+    LCD_CS_Set();
+
+	return recv_data;
+}
+
+
+/******************************************************************************
+      函数说明：LCD写入数据
+      入口数据：dat 写入的数据
+      返回值：  无
+******************************************************************************/
+void LCD_WR_DATA8(u8 dat)
+{
+	LCD_Writ_Bus(dat);
+}
+
+
+/******************************************************************************
+      函数说明：LCD写入数据
+      入口数据：dat 写入的数据
+      返回值：  无
+******************************************************************************/
+void LCD_WR_DATA(u16 dat)
+{
+	LCD_Writ_Bus(dat>>8);
+	LCD_Writ_Bus(dat);
+}
+
+
+/******************************************************************************
+      函数说明：LCD写入命令
+      入口数据：dat 写入的命令
+      返回值：  无
+******************************************************************************/
+void LCD_WR_REG(u8 dat)
+{
+	LCD_DC_Clr();//写命令
+	LCD_Writ_Bus(dat);
+	LCD_DC_Set();//写数据
+}
+
+
+/******************************************************************************
+      函数说明：设置起始和结束地址
+      入口数据：x1,x2 设置列的起始和结束地址
+                y1,y2 设置行的起始和结束地址
+      返回值：  无
+******************************************************************************/
+void LCD_Address_Set(u16 x1,u16 y1,u16 x2,u16 y2)
+{
+	if(USE_HORIZONTAL==0)
+	{
+		LCD_WR_REG(0x2a);//列地址设置
+		LCD_WR_DATA(x1);
+		LCD_WR_DATA(x2);
+		LCD_WR_REG(0x2b);//行地址设置
+		LCD_WR_DATA(y1+20);
+		LCD_WR_DATA(y2+20);
+		LCD_WR_REG(0x2c);//储存器写
+	}
+	else if(USE_HORIZONTAL==1)
+	{
+		LCD_WR_REG(0x2a);//列地址设置
+		LCD_WR_DATA(x1);
+		LCD_WR_DATA(x2);
+		LCD_WR_REG(0x2b);//行地址设置
+		LCD_WR_DATA(y1+20);
+		LCD_WR_DATA(y2+20);
+		LCD_WR_REG(0x2c);//储存器写
+	}
+	else if(USE_HORIZONTAL==2)
+	{
+		LCD_WR_REG(0x2a);//列地址设置
+		LCD_WR_DATA(x1+20);
+		LCD_WR_DATA(x2+20);
+		LCD_WR_REG(0x2b);//行地址设置
+		LCD_WR_DATA(y1);
+		LCD_WR_DATA(y2);
+		LCD_WR_REG(0x2c);//储存器写
+	}
+	else
+	{
+		LCD_WR_REG(0x2a);//列地址设置
+		LCD_WR_DATA(x1+20);
+		LCD_WR_DATA(x2+20);
+		LCD_WR_REG(0x2b);//行地址设置
+		LCD_WR_DATA(y1);
+		LCD_WR_DATA(y2);
+		LCD_WR_REG(0x2c);//储存器写
+	}
+}
+
+void LCD_Init(void)
+{
+    fsp_err_t err = R_SPI_Open(&g_spi0_ctrl, g_spi0.p_cfg);
+    if (err != FSP_SUCCESS)
+    {
+        printf("\r\nSPI启动错误\r\n");
+    }
+	
+	LCD_RES_Clr();//复位
+	delay_ms(100);
+	LCD_RES_Set();
+	delay_ms(100);
+	
+	LCD_BLK_Set();//打开背光
+  delay_ms(100);
+	
+	//************* Start Initial Sequence **********//
+	LCD_WR_REG(0x11); //Sleep out 
+	delay_ms(120);              //Delay 120ms 
+	//************* Start Initial Sequence **********// 
+	LCD_WR_REG(0x36);
+	if(USE_HORIZONTAL==0)LCD_WR_DATA8(0x00);
+	else if(USE_HORIZONTAL==1)LCD_WR_DATA8(0xC0);
+	else if(USE_HORIZONTAL==2)LCD_WR_DATA8(0x70);
+	else LCD_WR_DATA8(0xA0);
+
+	LCD_WR_REG(0x3A);			
+	LCD_WR_DATA8(0x05);
+
+	LCD_WR_REG(0xB2);			
+	LCD_WR_DATA8(0x0C);
+	LCD_WR_DATA8(0x0C); 
+	LCD_WR_DATA8(0x00); 
+	LCD_WR_DATA8(0x33); 
+	LCD_WR_DATA8(0x33); 			
+
+	LCD_WR_REG(0xB7);			
+	LCD_WR_DATA8(0x35);
+
+	LCD_WR_REG(0xBB);			
+	LCD_WR_DATA8(0x32); //Vcom=1.35V
+					
+	LCD_WR_REG(0xC2);
+	LCD_WR_DATA8(0x01);
+
+	LCD_WR_REG(0xC3);			
+	LCD_WR_DATA8(0x15); //GVDD=4.8V  颜色深度
+				
+	LCD_WR_REG(0xC4);			
+	LCD_WR_DATA8(0x20); //VDV, 0x20:0v
+
+	LCD_WR_REG(0xC6);			
+	LCD_WR_DATA8(0x0F); //0x0F:60Hz        	
+
+	LCD_WR_REG(0xD0);			
+	LCD_WR_DATA8(0xA4);
+	LCD_WR_DATA8(0xA1); 
+
+	LCD_WR_REG(0xE0);
+	LCD_WR_DATA8(0xD0);   
+	LCD_WR_DATA8(0x08);   
+	LCD_WR_DATA8(0x0E);   
+	LCD_WR_DATA8(0x09);   
+	LCD_WR_DATA8(0x09);   
+	LCD_WR_DATA8(0x05);   
+	LCD_WR_DATA8(0x31);   
+	LCD_WR_DATA8(0x33);   
+	LCD_WR_DATA8(0x48);   
+	LCD_WR_DATA8(0x17);   
+	LCD_WR_DATA8(0x14);   
+	LCD_WR_DATA8(0x15);   
+	LCD_WR_DATA8(0x31);   
+	LCD_WR_DATA8(0x34);   
+
+	LCD_WR_REG(0xE1);     
+	LCD_WR_DATA8(0xD0);   
+	LCD_WR_DATA8(0x08);   
+	LCD_WR_DATA8(0x0E);   
+	LCD_WR_DATA8(0x09);   
+	LCD_WR_DATA8(0x09);   
+	LCD_WR_DATA8(0x15);   
+	LCD_WR_DATA8(0x31);   
+	LCD_WR_DATA8(0x33);   
+	LCD_WR_DATA8(0x48);   
+	LCD_WR_DATA8(0x17);   
+	LCD_WR_DATA8(0x14);   
+	LCD_WR_DATA8(0x15);   
+	LCD_WR_DATA8(0x31);   
+	LCD_WR_DATA8(0x34);
+	LCD_WR_REG(0x21); 
+
+	LCD_WR_REG(0x29);
+} 
+
+
+
+
+
+
+
