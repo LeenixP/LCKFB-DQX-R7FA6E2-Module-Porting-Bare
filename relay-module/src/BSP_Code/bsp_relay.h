f62ccfe984288c8368d54545a948066df9d9043e
LeenixP <lipeng.git@qq.com>
2025-08-21 11:08:40 +0800

    更新【继电器模块】模块

---
+++ relay-module/src/BSP_Code/bsp_relay.h
@@ -0,0 +1,47 @@
+/*
+ * 立创开发板软硬件资料与相关扩展板软硬件资料官网全部开源
+ * 开发板官网：www.lckfb.com
+ * 文档网站：wiki.lckfb.com
+ * 技术支持常驻论坛，任何技术问题欢迎随时交流学习
+ * 嘉立创社区问答：https://www.jlc-bbs.com/lckfb
+ * 关注bilibili账号：【立创开发板】，掌握我们的最新动态！
+ * 不靠卖板赚钱，以培养中国工程师为己任
+ */
+
+#ifndef BSP_CODE_BSP_RELAY_H_
+#define BSP_CODE_BSP_RELAY_H_
+
+#include "hal_data.h"
+#include <stdio.h>
+
+#ifndef u8
+#define u8 uint8_t
+#endif
+#ifndef u16
+#define u16 uint16_t
+#endif
+#ifndef u32
+#define u32 uint32_t
+#endif
+
+#ifndef delay_ms
+#define delay_ms(x)   R_BSP_SoftwareDelay(x, BSP_DELAY_UNITS_MILLISECONDS)
+#endif
+#ifndef delay_1ms
+#define delay_1ms(x)  R_BSP_SoftwareDelay(x, BSP_DELAY_UNITS_MILLISECONDS)
+#endif
+#ifndef delay_us
+#define delay_us(x)   R_BSP_SoftwareDelay(x, BSP_DELAY_UNITS_MICROSECONDS)
+#endif
+#ifndef delay_1us
+#define delay_1us(x)  R_BSP_SoftwareDelay(x, BSP_DELAY_UNITS_MICROSECONDS)
+#endif
+
+
+#define BSP_RELAY_GPIO_PIN		BSP_IO_PORT_04_PIN_08
+#define RELAY_OUT(x)            R_IOPORT_PinWrite(&g_ioport_ctrl, BSP_RELAY_GPIO_PIN, x)
+
+void Set_Relay_Switch(unsigned char state);//设置继电器状态
+
+#endif /* BSP_CODE_BSP_RELAY_H_ */
+
